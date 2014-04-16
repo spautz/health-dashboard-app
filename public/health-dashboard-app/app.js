76f32e3b3866f5455eed546252ac651129866eb2
Steven Pautz <spautz@neonova.net>
2014-04-15 21:08:05 -0400

    Hello World, html with an app

---
+++ public/health-dashboard-app/app.js
@@ -0,0 +1,14 @@
+(function(angular) {
+    'use strict';
+
+    var healthDashboardApp = angular.module('healthDashboardApp', [
+        // no dependencies yet
+    ]);
+
+    healthDashboardApp.config([
+        function() {
+            console.log('Hello World');
+        }
+    ]);
+
+}(angular));
